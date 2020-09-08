9693764acb620442191d8c2b2ebb0f49ffd61136
ningpop <craft1933@naver.com>
2020-09-09 01:26:26 +0900

    understand api server

--- firstrest/post/serializer.py
+++ firstrest/post/serializer.py
@@ -4,5 +4,7 @@ from rest_framework import serializers
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        # fields = '__all__'
-        fields = ['title', 'body']
+        fields = '__all__'
+        # fields = ['id', 'title', 'body']
+        read_only_fields = ('title',)
+        # write_only_fields = ('title',)
