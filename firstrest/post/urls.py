5be9ea2a0aebbb8f851d1c3643bf15e670778624
ningpop <craft1933@naver.com>
2020-09-09 02:28:31 +0900

    APIView

--- firstrest/post/urls.py
+++ firstrest/post/urls.py
@@ -1,12 +1,28 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from . import views
 
-# django rest framework -> routet -> url
+# # django rest framework -> routet -> url
 
-router = DefaultRouter()
-router.register('post', views.PostViewset)
+# router = DefaultRouter()
+# router.register('post', views.PostViewset)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from post import views
+
+# Default Router 사용 X ==> API ROOT 없음. 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # 127.0.0.1:8000/post == ListView
+    path('post/', views.PostList.as_view()),  
+    # 127.0.0.1:8000/post/<pk> == DetailView
+    path('post/<int:pk>/', views.PostDetail.as_view()), 
 ]
+
+#
+urlpatterns = format_suffix_patterns(urlpatterns)
